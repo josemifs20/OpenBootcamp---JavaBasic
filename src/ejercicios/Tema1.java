7e50f2f20a4c5ee1de449ebf80cfd8a4ad0e9450
Josemi <fs.josemiguel@gmail.com>
2022-08-29 18:51:37 +0200

    Ejercicio Tema1

---
+++ src/ejercicios/Tema1.java
@@ -0,0 +1,13 @@
+package ejercicios;
+
+public class Tema1 {
+    public static void main(String[] args) {
+        int numero = 2;
+        long grande = 200;
+        double decimal = 20.20;
+        boolean verdad = true;
+        String name = "Tema1";
+        System.out.printf("El número es %d, el número grande es %d, el decimal o double es %.2f.%n" +
+                "Luego tenemos un %b, que se llama %s.%n",numero, grande, decimal, verdad, name);
+    }
+}
