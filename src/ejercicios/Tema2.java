b9221e07bc17e740da2141eca960d4f710006c6d
Josemi <fs.josemiguel@gmail.com>
2022-08-30 21:09:56 +0200

    Ejercicio Tema2

--- src/ejercicios/Tema2.java
+++ src/ejercicios/Tema2.java
@@ -1,10 +1,18 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Tema2 {
     public static void main(String[] args) {
         double precio = 100;
         double calculo = ivaPrecio(precio);
         System.out.println("El precio con Iva es de " + calculo);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Indica el precio al que sumar el IVA:");
+        double precioTeclado = scanner.nextDouble();
+        double calculoTeclado = ivaPrecio(precioTeclado);
+        System.out.println("El precio con Iva es de " + calculoTeclado);
     }
 
     public static double ivaPrecio (double precio) {
